4fc5f8ebb82eed65c0e7095e9f5b80a7363ce87a
monjur <monjurmorshed793@gmail.com>
2022-03-02 13:29:22 +0600

    Mongo tests added

--- src/main/java/gov/bd/banbeis/models/Navigation.java
+++ src/main/java/gov/bd/banbeis/models/Navigation.java
@@ -2,11 +2,27 @@ package gov.bd.banbeis.models;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @MongoEntity(collection = "Navigation")
 public class Navigation extends ReactivePanacheMongoEntity {
-    private String id;
-    private String route;
-    private String name;
-    private Navigation[] child;
+    public String route;
+    public String label;
+    public String icon;
+    public List<Navigation> child = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Navigation{" +
+                "route='" + route + '\'' +
+                ", label='" + label + '\'' +
+                ", icon='" + icon + '\'' +
+                ", child=" + child +
+                ", id=" + id +
+                '}';
+    }
 }
---
+++ src/test/java/gov/bd/banbeis/models/NavigationTest.java
@@ -0,0 +1,42 @@
+package gov.bd.banbeis.models;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@QuarkusTest
+public class NavigationTest {
+
+    @Test
+    public void testNormalInsertion(){
+        Navigation dashboard = new Navigation();
+        dashboard.route="/dashboard";
+        dashboard.label="dashboard";
+        dashboard.icon = "dashboard-icon";
+
+        Navigation employeeInfo = new Navigation();
+        employeeInfo.route="/employeeInfo";
+        employeeInfo.label="Personal Info";
+        employeeInfo.icon="personal-icon";
+
+        Navigation configuration = new Navigation();
+        configuration.route="/configuration";
+        configuration.label="configuration";
+        configuration.icon = "configuration-icon";
+        configuration.child = Arrays.asList(dashboard, employeeInfo);
+
+        Uni<Navigation> savedConfiguration = configuration.persistOrUpdate();
+        Navigation storedConfiguration = savedConfiguration.await().indefinitely();
+        System.out.println("Printing saved id");
+        System.out.println(storedConfiguration.id);
+        System.out.println(storedConfiguration.toString());
+        //Assertions.assertNotNull(configuration.id);
+    }
+}
