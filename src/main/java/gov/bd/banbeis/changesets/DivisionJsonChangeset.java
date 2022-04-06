4b81c7a2def1a07bc4793b50165a9c7f8c6478bd
monjur <monjurmorshed793@gmail.com>
2022-04-06 15:28:39 +0600

    Font related issue resolved (Bangla)

--- src/main/java/gov/bd/banbeis/changesets/DivisionJsonChangeset.java
+++ src/main/java/gov/bd/banbeis/changesets/DivisionJsonChangeset.java
@@ -1,62 +1,49 @@
 package gov.bd.banbeis.changesets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.bd.banbeis.models.Division;
-import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
 import liquibase.change.custom.CustomTaskChange;
 import liquibase.database.Database;
 import liquibase.exception.CustomChangeException;
 import liquibase.exception.SetupException;
 import liquibase.exception.ValidationErrors;
-import liquibase.repackaged.org.apache.commons.lang3.ClassLoaderUtils;
 import liquibase.resource.ResourceAccessor;
-import org.bson.types.ObjectId;
 
-import javax.inject.Inject;
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class DivisionChangeset implements CustomTaskChange {
+public class DivisionJsonChangeset implements CustomTaskChange {
 
-    // to hold the parameter value (csv file location)
     private String file;
-
     private ResourceAccessor resourceAccessor;
 
-    public String getFile(){
+    public String getFile() {
         return file;
     }
 
-    public void setFile(String file){
+    public void setFile(String file) {
         this.file = file;
     }
 
     @Override
-    @Blocking
     public void execute(Database database) throws CustomChangeException {
         try{
             BufferedReader in = new BufferedReader(
-                    new InputStreamReader(resourceAccessor.openStream(null, file), "UTF-8")
+                    new InputStreamReader(resourceAccessor.openStream(null, file))
             );
             //ignore header
-            String str = in.readLine();
+            String str = null;
             List<Division> divisions = new ArrayList<>();
             while ((str = in.readLine()) != null && !str.trim().equals("")) {
-                List<String> objects = new ArrayList<String>(Arrays.asList(str.split(",")));
-                Division division = new Division();
-                division.divisionId = objects.get(0);
-                division.name =  objects.get(1);
-                division.bnName = objects.get(2);
-                System.out.println(division.bnName);
-                division.url = objects.get(3);
+                ObjectMapper objectMapper = new ObjectMapper();
+                Division division = objectMapper.readValue(str, Division.class);
                 divisions.add(division);
-//                division.persist().subscribe().asCompletionStage();
             }
-            Division.persist(divisions).subscribeAsCompletionStage();
+            Division.persist(divisions).subscribe().asCompletionStage();
 
         }catch (Exception e){
             throw new CustomChangeException(e);
